c464f74fc8ab2b6ad00e38e78148b18f7ebfb550
Dominika Kawucha <dominika.kawucha@icloud.com>
2018-11-05 20:39:42 +0100

    init db

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .get('/db', async (req, res) => {
+  .get('/', async (req, res) => {
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      res.render('pages/index', results );
       client.release();
     } catch (err) {
       console.error(err);
